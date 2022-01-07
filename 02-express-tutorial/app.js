6f12167c446e9e884dccc983cc039d6547f9793b
Ronald Arias <ariasron@hotmail.com>
2022-01-06 21:13:54 -0500

    Returns a response with a code status and content type

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,8 +1,11 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  console.log('user hit the server')
-  res.end('Hello World')
+  res.writeHead(200, {
+    'Content-Type': 'text/html'
+  })
+  res.write('<h1>Hello World</h1>')
+  res.end()
 })
 
 server.listen(5000)
