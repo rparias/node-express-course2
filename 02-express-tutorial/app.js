b3d9dcb1486c6218f4fe667262db3a759d1f7eba
Ronald Arias <ariasron@hotmail.com>
2022-01-08 19:29:09 -0500

    Get the parameters from the request on a get http call

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -14,6 +14,17 @@ app.get('/api/products', (req, res) => {
   res.json(newProducts)
 })
 
+app.get('/api/products/:productId', (req, res) => {
+  const { productId } = req.params
+  const singleProduct = products.find(product => product.id === Number(productId))
+  
+  if(!singleProduct) {
+    res.status(404).send('Product does not exist')
+  }
+
+  res.json(singleProduct)
+})
+
 app.listen(5000, () => {
   console.log('Server is listening on port 5000');
 })
