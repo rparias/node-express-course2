687484bbc604ef6340bd6c299c8582969459e964
Ronald Arias <ariasron@hotmail.com>
2022-01-10 21:37:52 -0500

    creates a get a post endpoints to handle people

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,33 +1,25 @@
 const express = require('express')
 const app = express()
+const { people } = require('./data')
 
-// req => middleware => res
-const logger = require('./logger')
-const authorize = require('./authorize')
+// static assets
+app.use(express.static('./methods-public'))
 
-// Call the middleware for every endpoint instead of calling in the method as a second parameter
-// The app.use should be called before the endpoints, otherwise the middleware will be called only for the endpoints below it
-// app.use('/api', logger) this middleware will be executed only when the url has /api
-app.use([logger, authorize])
+// parse incoming requests with urlencoded payloads
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
-  res.send('Home')
+app.get('/api/people', (req, res) => {
+  res.status(200).json({success: true, data: people})
 })
 
-app.get('/about', (req, res) => {
-  res.send('About')
+app.post('/login', (req, res) => {
+  const { name } = req.body
+  if(name) {
+    return res.status(200).send(`Welcome ${name}`)
+  }
+  res.status(401).send('Please provide credentials')
 })
-
-app.get('/api/products', (req, res) => {
-  res.send('Products')
-})
-
-app.get('/api/items', (req, res) => {
-  console.log(req.user)
-  res.send('Items')
-})
-
 
 app.listen(5000, () => {
-  console.log('Server is listening on port 5000...');
+  console.log('Listening on port 5000...')
 })
