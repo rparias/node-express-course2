ca016920104b50cf6679c6e7ae04acb4221d4d37
Ronald Arias <ariasron@hotmail.com>
2022-01-11 21:19:46 -0500

    Creates a post endpoint for people and allows json requests

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -6,10 +6,21 @@ const { people } = require('./data')
 app.use(express.static('./methods-public'))
 
 // parse incoming requests with urlencoded payloads
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: false }))
+
+// parse json
+app.use(express.json())
 
 app.get('/api/people', (req, res) => {
   res.status(200).json({success: true, data: people})
+})
+
+app.post('/api/people', (req, res) => {
+  const { name } = req.body
+  if(!name) {
+    return res.status(400).json({success: false, msg: 'please provide name value'})
+  }
+  res.status(201).json({success: true, person: name})
 })
 
 app.post('/login', (req, res) => {
