399086e620bca55e8175350cd6908bdd3c5249a9
Ronald Arias <ariasron@hotmail.com>
2022-03-28 20:44:47 -0500

    Refactor code to set up Express Router

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const { people } = require('./data')
+const peopleRouter = require('./routes/people')
+const authRouter = require('./routes/auth')
 
 // static assets
 app.use(express.static('./methods-public'))
@@ -11,59 +12,12 @@ app.use(express.urlencoded({ extended: false }))
 // parse json
 app.use(express.json())
 
-app.get('/api/people', (req, res) => {
-  res.status(200).json({success: true, data: people})
-})
+// people router
+app.use('/api/people', peopleRouter)
 
-app.post('/api/people', (req, res) => {
-  const { name } = req.body
-  if(!name) {
-    return res.status(400).json({success: false, msg: 'please provide name value'})
-  }
-  people.push({id: people.length + 1, name})
-  res.status(201).json({success: true, data: people})
-})
+// auth router
+app.use('/api/login', authRouter)
 
-app.put('/api/people/:id', (req, res) => {
-  const { id } = req.params
-  const { name } = req.body
-  
-  const person = people.find((person) => person.id === Number(id))
-  if(!person) {
-    return res.status(400).json({success: false, msg: `no person with id: ${id}`})
-  }
-
-  const newPeople = people.map((person) => {
-    if(person.id === Number(id)) {
-      person.name = name
-    }
-    return person
-  })
-
-  res.status(200).json({success: true, data: newPeople})
-})
-
-app.delete('/api/people/:id', (req, res) => {
-  const { id } = req.params
-  const person = people.find(person => person.id === Number(id))
-
-  if(!person) {
-    return res
-      .status(404)
-      .json({ success: false, msg: `no person with id ${id}`})
-  }
-
-  const newPeople = people.filter(person => person.id !== Number(id))
-  return res.status(200).json({ success: true, data: newPeople })
-})
-
-app.post('/login', (req, res) => {
-  const { name } = req.body
-  if(name) {
-    return res.status(200).send(`Welcome ${name}`)
-  }
-  res.status(401).send('Please provide credentials')
-})
 
 app.listen(5000, () => {
   console.log('Listening on port 5000...')
